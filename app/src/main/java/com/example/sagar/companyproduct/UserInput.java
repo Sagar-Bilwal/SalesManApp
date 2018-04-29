65c601acb4b10d778065bfa9de9d69fc6b2e5fc9
SAGAR <bilwal.sagar@gmail.com>
2018-04-29 23:43:27 +0530

    App Completed ,UI left

--- app/src/main/java/com/example/sagar/companyproduct/UserInput.java
+++ app/src/main/java/com/example/sagar/companyproduct/UserInput.java
@@ -1,6 +1,7 @@
 package com.example.sagar.companyproduct;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +13,17 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.sagar.companyproduct.model.TaskNo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
+import java.util.Objects;
 import java.util.Random;
 
 public class UserInput extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener{
@@ -24,8 +34,10 @@ public class UserInput extends AppCompatActivity implements DatePickerDialog.OnD
     RadioButton yes;
     RadioButton no;
     EditText reason;
+    String taskNo,address1,contact,name,company_name;
     int final_day ,final_month,final_year;
     Button save;
+    String status;
     int year,month,day;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +52,12 @@ public class UserInput extends AppCompatActivity implements DatePickerDialog.OnD
         save=findViewById(R.id.user_input_save);
         Intent intent=getIntent();
         Bundle bundle=intent.getExtras();
-        String company_name=bundle.getString("COMPANY");
-        String taskNo=bundle.getString(CONSTANTS.TASK_NO);
-        String address1=bundle.getString("ADDRESS");
-        String contact=bundle.getString("CONTACT");
-        String name=bundle.getString("NAME");
+        company_name=bundle.getString("COMPANY");
+        taskNo=bundle.getString(CONSTANTS.TASK_NO);
+        address1=bundle.getString("ADDRESS");
+        contact=bundle.getString("CONTACT");
+        name=bundle.getString("NAME");
+        String Reason=" ";
         companyName.setText(company_name);
         address.setText(address1);
         date.setOnClickListener(new View.OnClickListener() {
@@ -58,25 +71,62 @@ public class UserInput extends AppCompatActivity implements DatePickerDialog.OnD
                 datePickerDialog.show();
             }
         });
+        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference=firebaseDatabase.getReference("Tasks");
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final ProgressDialog progressDialog = new ProgressDialog(UserInput.this);
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+                if (no.isChecked())
+                {
+                    status = "NO";
+                } else if (yes.isChecked())
+                {
+                    status = "YES";
+                } else {
+                    status = " ";
+                }
+                Toast.makeText(UserInput.this, reason.getText().toString(), Toast.LENGTH_SHORT).show();
+                if (no.isChecked() && Objects.equals(reason.getText().toString(), "")) {
+                    progressDialog.dismiss();
+                    Toast.makeText(UserInput.this, "Enter the Reason(Why was Meeting Unsuccessful ?)", Toast.LENGTH_SHORT).show();
+                } else if (date.getText().toString() == "") {
+                    progressDialog.dismiss();
+                    Toast.makeText(UserInput.this, "Enter the date of your Visit", Toast.LENGTH_SHORT).show();
+                } else {
+                    progressDialog.dismiss();
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            TaskNo Task = new TaskNo(company_name, address1, contact, status, reason.getText().toString()+" ", date.getText().toString(), name, taskNo + "");
+                            databaseReference.child(taskNo+" "+name).setValue(Task);
+                            Toast.makeText(UserInput.this, "Saved Successfully", Toast.LENGTH_LONG).show();
+                            databaseReference.removeEventListener(this);
+                            Intent intent = new Intent(UserInput.this, MainUserActivity.class);
+                            startActivity(intent);
+                        }
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Toast.makeText(UserInput.this, "Error Occurred,Saving Failed", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
             }
         });
     }
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                final_year = year;
+                final_month = month + 1;
+                final_day = dayOfMonth;
+                date.setText("" + final_day + "/" + final_month + "/" + final_year);
+            }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth)
-    {
-        final_year=year;
-        final_month=month+1;
-        final_day=dayOfMonth;
-        date.setText(""+final_day+"/"+final_month+"/"+final_year);
-    }
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-    }
+            }
 }
